2c8ee601a0e07f0c4981af33a67ea868ace77073
João Félix <joao.felix@yapily.com>
2022-09-07 13:51:30 +0100

    Adds MovieInfoRepository and integration tests

---
+++ movies-info-service/src/main/java/com/reactivespring/domain/MovieInfo.java
@@ -0,0 +1,30 @@
+package com.reactivespring.domain;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class MovieInfo {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private Integer year;
+
+    private List<String> cast;
+
+    private LocalDate releaseDate;
+
+}
---
+++ movies-info-service/src/test/java/integration/com/reactivespring/repository/MovieInfoRepositoryIntegrationTest.java
@@ -0,0 +1,113 @@
+package com.reactivespring.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.reactivespring.domain.MovieInfo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@DataMongoTest
+@ActiveProfiles("test")
+class MovieInfoRepositoryIntegrationTest {
+
+    @Autowired
+    MovieInfoRepository movieInfoRepository;
+
+    @BeforeEach
+    void setUp() {
+        var movieinfos = List.of(
+                new MovieInfo(null, "Batman Begins", 2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
+                new MovieInfo(null, "The Dark Knight", 2008, List.of("Christian Bale", "HeathLedger"), LocalDate.parse("2008-07-18")),
+                new MovieInfo("abc", "Dark Knight Rises", 2012, List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20"))
+        );
+
+        movieInfoRepository.saveAll(movieinfos)
+                           .blockLast();
+    }
+
+    @AfterEach
+    void tearDown() {
+        movieInfoRepository.deleteAll()
+                           .block();
+    }
+
+    @Test
+    void findAll() {
+        Flux<MovieInfo> flux = movieInfoRepository.findAll()
+                                                  .log();
+
+        StepVerifier.create(flux)
+                    .expectNextCount(3)
+                    .verifyComplete();
+    }
+
+    @Test
+    void findById() {
+        Mono<MovieInfo> mono = movieInfoRepository.findById("abc")
+                                                  .log();
+
+        StepVerifier.create(mono)
+                    .assertNext(movieInfo -> {
+                        assertEquals("Dark Knight Rises", movieInfo.getName());
+                    })
+                    .verifyComplete();
+    }
+
+    @Test
+    void saveMovieInfo() {
+        MovieInfo movieInfo = new MovieInfo(null, "Batman Begins 1", 2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15"));
+
+        Mono<MovieInfo> mono = movieInfoRepository.save(movieInfo);
+
+        StepVerifier.create(mono)
+                    .assertNext(mi -> {
+                        assertNotNull(mi.getId());
+                        assertEquals("Batman Begins 1", mi.getName());
+                    })
+                    .verifyComplete();
+    }
+
+    @Test
+    void updateMovieInfo() {
+        int year = 2020;
+
+        MovieInfo movieInfo = movieInfoRepository.findById("abc")
+                                                 .log()
+                                                 .block();
+        movieInfo.setYear(year);
+
+        Mono<MovieInfo> mono = movieInfoRepository.save(movieInfo);
+
+        StepVerifier.create(mono)
+                    .assertNext(mi -> {
+                        assertEquals(year, mi.getYear());
+                    })
+                    .verifyComplete();
+    }
+
+    @Test
+    void deleteMovieInfo() {
+        movieInfoRepository.deleteById("abc")
+                           .log()
+                           .block();
+
+        Flux<MovieInfo> flux = movieInfoRepository.findAll()
+                                                 .log();
+
+        StepVerifier.create(flux)
+                    .expectNextCount(2)
+                    .verifyComplete();
+    }
+}
